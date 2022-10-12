cd73f18d33f71b788efe8feb2ce675296f96743e
gearhead8764 <31350954+gearhead8764@users.noreply.github.com>
2022-10-12 11:02:31 -0400

    Add files via upload

---
+++ mosaic-rtsp.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+exec /usr/bin/vlc-wrapper rtsp://172.17.201.40:5004/cctv.sdp
---
+++ startup-mosaic-broadcast.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+exec screen /usr/bin/vlc -I "dummy" "$@" --vlm-conf /home/survtech/mosaic/hotel6.vlm
